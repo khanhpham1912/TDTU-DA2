7fe814345db1563df14accb8a01b64e67e8bda5b
khanhpham1912 <51800880@student.tdtu.edu.vn>
2023-12-23 00:26:14 +0700

    remove eslint in every service

--- apps/wms-service/src/modules/inbounds/dto/finding.option.inbound.dto.ts
+++ apps/wms-service/src/modules/inbounds/dto/finding.option.inbound.dto.ts
@@ -1,7 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
-import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
-import { Status } from 'src/enums/status.enum';
+import { ApiProperty } from "@nestjs/swagger";
+import { Expose } from "class-transformer";
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Status } from "src/enums/status.enum";
 
 export class FindingOptionInboundDto {
   @ApiProperty()
