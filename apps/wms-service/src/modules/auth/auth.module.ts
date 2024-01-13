0331b5ae6751eb0f91fb3260963c0c47b5779e94
Phạm Vũ Long Khải <khai.pham@wareflex.io>
2024-01-13 15:36:59 +0700

    update

--- apps/wms-service/src/modules/auth/auth.module.ts
+++ apps/wms-service/src/modules/auth/auth.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { UsersModule } from '../users/users.module';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { UsersModule } from "../users/users.module";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 @Module({
   imports: [
@@ -10,7 +10,7 @@ import { AuthService } from './auth.service';
     JwtModule.register({
       global: true,
       secret: `${process.env.SECRET}`,
-      signOptions: { expiresIn: '3600s' },
+      signOptions: { expiresIn: "360000s" },
     }),
   ],
   providers: [AuthService],
